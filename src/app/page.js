da0016a1adbddd59c2c89e093234a4310cb206a1
DarkAsfu <ashraful.islam.asfu@gmail.com>
2024-08-14 04:47:22 +0600

    resolve vercel deploy issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,6 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react/display-name */
+
 'use client'
 import Advertisement from '@/Components/Advertisement/Advertisement';
 import Banner from '@/Components/Banner/Banner';
@@ -14,45 +17,37 @@ import Preloader from '@/Components/Preloader/Preloader';
 const page = () => {
   useEffect(() => {
     AOS.init({
-      // Global settings:
-      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-      initClassName: 'aos-init', // class applied after initialization
-      animatedClassName: 'aos-animate', // class applied on animation
-      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-
-      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-      offset: 120, // offset (in px) from the original trigger point
-      delay: 0, // values from 0 to 3000, with step 50ms
-      duration: 400, // values from 0 to 3000, with step 50ms
-      easing: 'ease', // default easing for AOS animations
-      once: false, // whether animation should happen only once - while scrolling down
-      mirror: false, // whether elements should animate out while scrolling past them
-      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
+      disable: false,
+      startEvent: 'DOMContentLoaded',
+      initClassName: 'aos-init',
+      animatedClassName: 'aos-animate',
+      useClassNames: false,
+      disableMutationObserver: false,
+      debounceDelay: 50,
+      throttleDelay: 99,
+      offset: 120,
+      delay: 0,
+      duration: 400,
+      easing: 'ease',
+      once: false,
+      mirror: false,
+      anchorPlacement: 'top-bottom',
     });
-
   }, []);
+
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Disable scrolling when loading
     if (loading) {
       document.body.style.overflow = 'hidden';
     } else {
       document.body.style.overflow = 'auto';
     }
 
-    // Simulate a loading delay
     const timer = setTimeout(() => {
       setLoading(false);
     }, 2000);
 
-    // Cleanup to restore scrolling when component unmounts or loading ends
     return () => {
       document.body.style.overflow = 'auto';
       clearTimeout(timer);
@@ -73,7 +68,6 @@ const page = () => {
         </div>
       }
     </>
-
   );
 };
 
